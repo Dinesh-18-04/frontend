d39fca7d0b3e38396586c554197e598635bc3cff
Dinesh-18-04 <167107244+Dinesh-18-04@users.noreply.github.com>
2024-09-25 22:32:52 +0530

    vitee

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-export default {
-  build: {
-    rollupOptions: {
-      external: ['react-icons/io', 'react-icons/fa6', 'react-icons/fa']
-    }
-  }
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
